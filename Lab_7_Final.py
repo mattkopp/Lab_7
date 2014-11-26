457e8799298af67b82c17276434979d22e92041b
Matt Koppelman <mattkopp@uw.edu>
2014-11-25 17:06:29 -0800

    Update Script
    This script is way better, removed old scripts

--- Lab_7_Final.py
+++ Lab_7_Final.py
@@ -1,6 +1,7 @@
-#Matt Koppelman
-#GIS501
+#Name: Matt Koppelman
 #Lab 7
+
+
 
 import TwitterSearch
 from geopy import geocoders
@@ -13,16 +14,20 @@ import string
 import os
  
 
-env.workspace = r"C:\Users\Matt\Documents\UWTacoma\GIS501\GitHub\Lab_7\TwitterApp\results"
+env.workspace = r"C:\Users\Matt\Documents\UWTacoma\GIS501\GitHub\FinalProject\Results"
 arcpy.env.overwriteOutput = True
 
+try:
+	def geo(location):
+		g = geocoders.GoogleV3()
+		loc = g.geocode(location)
+		return loc.latitude, loc.longitude
 
-def geo(location):
-    g = geocoders.GoogleV3()
-    loc = g.geocode(location)
-    return loc.latitude, loc.longitude
+except GeocoderQuotaExceeded as q:
+	print(q)		
+		
+		
 
-#Dybbuugb
 try:
     tso = TwitterSearchOrder() # create a TwitterSearchOrder object
     tso.set_keywords(['ebfg']) # let's define all words we would like to have a look for
@@ -43,27 +48,40 @@ try:
             #print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text']))
             (lat, lng) = geo(tweet['place']['full_name'])
             CoordList.append((float(lat), float(lng)))	
-            print '(' + str(lat) +', ' +str(lng)+')'
-	#print CoordList
-	
-	
-	csvfile = r"C:\Users\Matt\Documents\UWTacoma\GIS501\GitHub\Lab_7\TwitterApp\results\ebfg.csv"
-	csvfields = ["Originalxy"]
-	with open (csvfile, 'wb') as field:
-		writer = csv.DictWriter(field, csvfields, delimiter = ',')
-		writer.writeheader()
-	
-	with open(csvfile, "a") as output:
-		writer = csv.writer(output, lineterminator='\n')
-		for val in CoordList:
-			writer.writerow([val])
+            #print '(' + str(lat) +', ' +str(lng)+')'
+	print CoordList
 	
 
-
 	
-
-
 except TwitterSearchException as e: # take care of all those ugly errors if there are some
     print(e)
+except GeocoderQuotaExceeded as q:
+	print(q)
+except GeocoderTimedOut as g:
+	print(g)
+#execfile(r"C:\Users\Matt\Documents\UWTacoma\GIS501\GitHub\Lab_7\TwitterApp\Lab7_Final_Split.py")
 
-execfile(r"C:\Users\Matt\Documents\UWTacoma\GIS501\GitHub\Lab_7\TwitterApp\Lab7_Final_Split.py")
+
+proj = 4326
+sr = arcpy.SpatialReference(proj)	
+	
+arcpy.CreateFeatureclass_management(r"C:\Users\Matt\Documents\UWTacoma\GIS501\GitHub\FinalProject\Results", "ebfg", "POINT","","","", "")
+fc = (r"C:\Users\Matt\Documents\UWTacoma\GIS501\GitHub\FinalProject\Results\ebfg.shp")	
+	
+	
+cursor = arcpy.da.InsertCursor(fc, ["SHAPE@"])
+p = arcpy.Point()
+pointlist = []
+	
+for pt in CoordList:
+	p.X = pt[1]
+	p.Y = pt[0]
+	pointlist.append(arcpy.PointGeometry(p))
+		
+	for coords in pointlist:
+		if coords == []:
+			continue
+		else:
+			arcpy.CopyFeatures_management(pointlist, fc)
+			arcpy.DefineProjection_management(fc, sr)
+arcpy.AddXY_management(fc)
